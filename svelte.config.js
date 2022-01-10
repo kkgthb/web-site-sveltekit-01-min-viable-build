ef76c4e96086c10b16d3ffc1dee7355c4c6ef2e0
kkgthb <44843658+kkgthb@users.noreply.github.com>
2022-01-10 13:56:12 -0600

    OK what if I use adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,15 @@
-// Svelte runs as long as you export at least an empty object, 
-// but you probably will want to configure fancy things later.
-const config = {};
+// Svelte runs locally as long as you export at least an empty object,
+// but as soon as you try to build on, say, Netlify, you're going to need
+// to add to it so as to pass in an "adapter."
+
+import adapter from "@sveltejs/adapter-auto";
+
+const config = {
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    //target: "#svelte",
+    adapter: adapter(),
+  },
+};
 
 export default config;
